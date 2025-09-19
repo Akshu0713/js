c9e9699d8e4f8621dcf99211615a1d0ff8c16429
Akshat Gupta <110551648+Akshu0713@users.noreply.github.com>
2025-09-19 11:12:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3)               // code readbhility should be high
+console.log("Akshat")
+
+
+let name = "Akshat" //string
+let age = 23 //number
+let isLoggedIn = false // boolean
+let state; //undefined
+
+// number => 2 power 53
+// bigint
+// string => "", ''
+// boolean => true/false
+// null (standalone value) 
+// undefined => the value has not been assigned yet
+// symbol => unique
+
+//object => 
+
+console.log(typeof "Akshat");
+console.log(typeof age);
+console.log(typeof null); // null is an object dataType
+console.log(typeof undefined); //o/p: undefined (dataType)
