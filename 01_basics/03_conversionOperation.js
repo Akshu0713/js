8abbb997c1f8db53d8744154ddedad559bd19cf4
Akshat Gupta <110551648+Akshu0713@users.noreply.github.com>
2025-09-21 13:15:31 +0000

    ConversionOperation Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -50,7 +50,59 @@ let booleanIsLoggedIn2 = Boolean(isLoggedIn2);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2**3); //power **
+// console.log(2/3); 
+// console.log(2%3); //remainder
+
+let str1 = "Hello"
+let str2 = " Akshat"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2) // conversion has happened inside string
+// console.log(1 + 2 + "2") //first conversion based on numbers 1 + 2 = 3 then 
+                            // conversion based on string which gives o/p: 32
+// console.log( (3 + 4) * 5 % 3);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); // gives error
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter; //prefix
+gameCounter++; //postfix
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
 
 
