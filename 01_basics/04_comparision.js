14f6bd5360cc4e4142faf37cd9273ecfe7cc3d18
Akshat Gupta <110551648+Akshu0713@users.noreply.github.com>
2025-09-23 05:14:48 +0000

    Done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,17 +8,17 @@
                         // so that the comparison and predictable result is not there.
 // console.log("02" > 1);
 
-// console.log(null > 0); // o/p: false
-// console.log(null == 0); // o/p: false
-// console.log(null >= 0); // o/p: true
+console.log(null > 0); // o/p: false
+console.log(null == 0); // o/p: false
+console.log(null >= 0); // o/p: true
 
-// /*The reason is that an equality check == and comparisons > ,< ,>= ,<= work differently.
-// Comparisons convert null to a number, treating it as 0.
-// That's why (3) null >= 0 is true and (1) null > 0 is false.*/
+/*The reason is that an equality check == and comparisons > ,< ,>= ,<= work differently.
+Comparisons convert null to a number, treating it as 0.
+That's why (3) null >= 0 is true and (1) null > 0 is false.*/
 
-// console.log(undefined == 0); // o/p: false
-// console.log(undefined > 0); // o/p: false
-// console.log(undefined < 0); // o/p: false
+console.log(undefined == 0); // o/p: false
+console.log(undefined > 0); // o/p: false
+console.log(undefined < 0); // o/p: false
 
 // === (strict check) it checks both value and dataType 
 
