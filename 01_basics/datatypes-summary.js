318974d825cc8490a46a6f499e77844e21c6055f
Akshat Gupta <110551648+Akshu0713@users.noreply.github.com>
2025-09-23 05:17:36 +0000

    DataTypes done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null // temperature is empty.
+let userEmail; //initially undefined
+
+const id = Symbol('123') //The JavaScript Symbol is a primitive data type, just like Number, String, Boolean, etc. 
+                         // It represents a unique identifier and can be used in various ways.
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
